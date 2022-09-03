78b31f5c2ddde92d5244ff6ec58951886c79bf64
Sachin <soniji7269@gmail.com>
2022-09-03 22:59:04 +0530

    maths-operators

--- masai/problem2.js
+++
@@ -1,9 +0,0 @@
-let name = "Sachin Verma";
-console.log(name);
-
-let FatherName = "Pawan Verma";
-console.log(FatherName);
-
-let MotherName = "Usha Verma";
-console.log(MotherName);
-
--- masai/problem3.js
+++
@@ -1,6 +0,0 @@
-let name = "Sachin Verma";
-let age = 22;
-
-
-console.log(name +" " +age);
-console.log(typeof(name)+" "+typeof(age));
